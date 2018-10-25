bba792818b7dc440f06590c027c50189c5ce79c3
PLUCHE Cyril <cyril.pluche@etu.umontpellier.fr>
2018-10-25 10:19:11 +0200

    [CLIENT] - New client folder REDUX/ASYNC OK (All files)

---
+++ client-new/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Dashboard from './dashboard/Dashboard'
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            Hello React
+            <Dashboard />
+        </div>
+    );
+  }
+}
+
+export default App;
