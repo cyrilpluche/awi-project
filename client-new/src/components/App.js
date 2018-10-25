1fcf1c7d5cf21a099700c3df56a30b7a604b8aa5
Mehdqtx <Mehdi.delvaux@gmail.com>
2018-10-25 16:24:30 +0200

    App router

--- client-new/src/components/App.js
+++ client-new/src/components/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import Signin from './signin/Signin'
 import Dashboard from './dashboard/Dashboard'
+import {  BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-        <div>
-            Hello React
-            <Dashboard />
-        </div>
+      <Router>
+        <Switch>
+            <Route exact path="/" component={Signin}/>
+            <Route exact path="/home" component={Dashboard}/>
+            <Route component={Signin}/>
+        </Switch>
+      </Router>
     );
   }
 }
