a09bcdeba92cd8f884a5902449fa3ea0d280c1fe
Mehdqtx <Mehdi.delvaux@gmail.com>
2018-10-29 21:40:58 +0100

    [Project] Add card service

---
+++ client/src/services/Card.service.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import helper from '../helpers'
+axios.defaults.headers.common['Authorization'] = localStorage.getItem('memberToken')
+
+
+const url = 'http://localhost:4200/api/card/'
+
+const Card = {
+
+    getAll () {
+        return axios.get(url + 'find_all').then(res => res.data)
+    },
+
+    get (object) {
+        var where = helper.Request.urlFromObject(object)
+        return axios.get(url + 'find_one' + where).then(res => res.data)
+    },
+    create(body){
+        return axios.post(url + 'create', body).then(res => res.data)
+    }
+
+}
+
+export default Card
