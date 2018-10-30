86139017af4536b5ac26b53bd0e8f73b8dd85dfe
Amin bz <amin.bazaz@etu.umontpellier.fr>
2018-10-30 12:12:17 +0100

    [card] #8 - Client side version 2 card without store

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -11,6 +11,10 @@ import Avatar from "@material-ui/core/Avatar/Avatar";
 import Button from "@material-ui/core/Button/Button";
 import { Scrollbars } from 'react-custom-scrollbars';
 import CardHeader from "@material-ui/core/CardHeader/CardHeader";
+import AddIcon from '@material-ui/icons/Add';
+import SvgIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import Divider from "@material-ui/core/Divider/Divider";
+import TextField from "@material-ui/core/TextField/TextField";
 
 
 class Cardboard extends React.Component {
@@ -48,46 +52,65 @@ class Cardboard extends React.Component {
                             transform: `translate(-50%, -50%)`
                         }
                     }>
-                    <div className={classes.paper}>
-                        <div className={classes.column} style={{ borderRight: '0.1em solid grey'}}>
-                            <Typography variant="h5" id="modal-title">
-                                Clean the github
-                            </Typography>
-                            <div className={classes.row}>
-                                <h4 >Members : </h4>
-                                <Avatar className={classes.marginCard}>H</Avatar>
-                            </div>
-                            <div className={classes.row}>
-                                <h4>Labels : </h4>
-                                <Button variant="outlined" color="secondary" className={classes.marginCard}>Label</Button>
+                        <div className={classes.paper}>
+                            <div className={classes.column} style={{ borderRight: '0.1em solid grey'}}>
+                                <Typography variant="h5" id="modal-title">
+                                    Clean the github
+                                </Typography>
+                                <Divider className={classes.divider}/>
+                                <div className={classes.row}>
+                                    <h4 >Members : </h4>
+                                    <Avatar className={classes.marginCard}>H</Avatar>
+                                    <Button variant="fab" color="primary" aria-label="Add" className={classes.buttonIcon} ><AddIcon/></Button>
+                                </div>
+                                <Divider className={classes.divider}/>
+                                <div className={classes.row}>
+                                    <h4>Labels : </h4>
+                                    <Button disabled className={classes.buttonLabel}>Backend</Button>
+                                    <Button variant="fab" color="primary" aria-label="Add" className={classes.buttonIcon} ><AddIcon/></Button>
+                                </div>
+                                <Divider className={classes.divider}/>
+                                <div>
+                                    <div className={classes.row}>
+                                        <h4>Description</h4>
+                                        <button className={classes.editButton}><SvgIcon>{<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>}</SvgIcon></button>
+                                    </div>
+                                    <TextField className={classes.textArea}
+                                               value="************* *********** ********* ********** ******* *********** ********** ************* ***"
+                                               multiline={true}
+                                               rows={2}
+                                               rowsMax={10}
+                                    />
+                                </div>
+                                <Divider className={classes.divider}/>
+                                <div>
+                                    <div className={classes.row}>
+                                        <h4>Comment</h4>
+                                        <button className={classes.editButton}><SvgIcon>{<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>}</SvgIcon></button>
+                                    </div>
+                                    <TextField className={classes.textArea}
+                                               value="************* *********** ********* ********** ******* *********** ********** ************* ***"
+                                               multiline={true}
+                                               rows={2}
+                                               rowsMax={10}
+                                    />
+                                </div>
+                                <Divider className={classes.divider}/>
                             </div>
                             <div>
-                                <h4>Description</h4>
-                                <Typography component="p">
-                                    Add your description ...
-                                </Typography>
-                            </div>
-                            <div>
-                                <h4>Comment</h4>
-                                <Typography component="p">
-                                    Add your comment ...
-                                </Typography>
+                                <Button variant="contained" className={classes.buttonModal}>Members</Button>
+                                <Button variant="contained" className={classes.buttonModal}>Labels</Button>
+                                <Button variant="contained" className={classes.buttonModal}>Checklist</Button>
+                                <Button variant="contained" className={classes.buttonModal}>Description</Button>
+                                <Button variant="contained" className={classes.buttonModal}>Comment</Button>
+                                <Button variant="contained" className={classes.buttonModal}>Attachment</Button>
+                                <Button variant="contained" className={classes.buttonModal}>Copy</Button>
+                                <Button variant="contained" className={classes.buttonModal}>Archive</Button>
+                                <Button variant="contained" className={classes.buttonModal}>Delete</Button>
                             </div>
                         </div>
-                        <div>
-                            <Button variant="contained" className={classes.button}>Members</Button>
-                            <Button variant="contained" className={classes.button}>Labels</Button>
-                            <Button variant="contained" className={classes.button}>Checklist</Button>
-                            <Button variant="contained" className={classes.button}>Description</Button>
-                            <Button variant="contained" className={classes.button}>Comment</Button>
-                            <Button variant="contained" className={classes.button}>Attachment</Button>
-                            <Button variant="contained" className={classes.button}>Copy</Button>
-                            <Button variant="contained" className={classes.button}>Archive</Button>
-                            <Button variant="contained" className={classes.button}>Delete</Button>
-                        </div>
-                    </div>
                     </Scrollbars>
-            </Modal>
+                </Modal>
                 <Card className={classes.card} >
                     <CardActionArea onClick={this.handleOpen}>
                         <CardHeader
@@ -99,7 +122,7 @@ class Cardboard extends React.Component {
                                 By merging all branches update the master branch
                             </Typography>
                             <div className={classes.rowRight}><Avatar className={classes.marginCard}>H</Avatar></div>
-                            <Button variant="outlined" color="secondary" className={classes.marginCard}>Label</Button>
+                            <Button disabled className={classes.buttonLabel}>Backend</Button>
                         </CardContent>
                     </CardActionArea>
                 </Card>
