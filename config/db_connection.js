585e9fb975f9490d5265d0a95a1c536cd1be5956
Pluchezerrr <cyril.pluche@etu.umontpellier.fr>
2018-11-11 14:24:30 +0100

    [HEROKU] - SSL DB fixed

--- config/db_connection.js
+++ config/db_connection.js
@@ -2,7 +2,10 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOSTNAME,
     dialect: 'postgres',
-    operatorsAliases: false
+    operatorsAliases: false,
+    dialectOptions: {
+        ssl: true
+    }
 })
 
 sequelize
