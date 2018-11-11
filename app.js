72fc907d96656e59a05a05f97aeb9476e4e78adb
Pluchezerrr <cyril.pluche@etu.umontpellier.fr>
2018-11-11 17:10:53 +0100

    [HEROKU] - Link build & express test1

--- app.js
+++ app.js
@@ -25,8 +25,9 @@ app.use(cors())
 
 // Use React App
 if (process.env.NODE_ENV === 'production') {
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    app.use(express.static(path.resolve(__dirname, './client/build')));
+    app.get('*', function(request, response) {
+        response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
     });
 }
 //app.use(express.static(path.join(__dirname, 'client/build')));
