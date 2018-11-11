a4c613e6a3d4f2954e40f54aed33c197a982a1b8
Pluchezerrr <cyril.pluche@etu.umontpellier.fr>
2018-11-11 14:30:13 +0100

    [HEROKU] - Server side OK -- Client side test1

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
 // Use React App
-/*if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build/index.html'));
     });
-}*/
+}
 /*app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build/index.html'));
